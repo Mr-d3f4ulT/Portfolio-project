4d8cab84bf100aafb10fc165f0776bf7c5aa62fa
Shivansh Pandey <yashpandey.1307@gmail.com>
2026-05-05 06:43:02 +0530

    Added some static constants in index.js

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { words } from "../constants";
 
 const Hero = () => {
   return (
@@ -13,7 +14,26 @@ const Hero = () => {
           <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
             <div className="flex flex-col gap-7">
               <div className="hero-text">
-                <h1>Shaping</h1>
+                <h1>
+                  Shaping
+                  <span className="slide">
+                    <span className="wrapper">
+                      {words.map((word, index) => (
+                        <span
+                          key={index}
+                          className="flex items-center md:gap-3 gap-1 pb-2"
+                        >
+                          <img
+                            src={word.imgPath}
+                            alt="person"
+                            className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                          />
+                          <span>{word.text}</span>
+                        </span>
+                      ))}
+                    </span>
+                  </span>
+                </h1>
                 <h1>into Real Projects</h1>
                 <h1>that Deliver Results</h1>
               </div>
